603d7df222c7c6602c8204c06152752549cac7bf
sencheele <sencheele@gmail.com>
2024-12-07 14:11:49 +0300

    Create TermCard

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,26 @@
 import './index.css';
+import { TermCard } from './TermCard.jsx';
+import { createRoot } from 'react-dom/client';
+
+const descriptionList = document.getElementById('description-list');
+
+// Инициализируем "корень" React-приложения. Это точка, где React будет контролировать DOM.
+// Передаём в него descriptionList, чтобы React знал, в какой элемент вставлять компоненты.
+const reactRoot = createRoot(descriptionList);
+
+// Вызываем метод render у корня, чтобы отрисовать компонент TermCard.
+// Компонент TermCard будет добавлен внутрь элемента description-list в DOM.
+reactRoot.render(<TermCard />);
+
+const form = document.getElementById('add-description');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const title = form.elements['title'].value;
+  const description = form.elements['description'].value;
+
+  form.reset();
+
+  console.log(title, description);
+})
